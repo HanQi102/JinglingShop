1c2bcf068e90aa74407c72c32a4510bad9171e72
hanqi <2331129256@qq.com>
2024-01-26 19:36:35 +0800

    登录页基本完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,90 @@
 <template>
     <div class="LoginBox">
-        123
+        <!-- 登录表单 -->
+        <el-form ref="loginFormRef" :model="loginRuleForm" :rules="LoginRules">
+            <el-form-item prop="username">
+                <el-input :prefix-icon="Search" v-model="loginRuleForm.username" clearable placeholder="请输入用户名" />
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input :prefix-icon="Search" v-model="loginRuleForm.password" clearable show-password
+                    placeholder="请输入密码" />
+            </el-form-item>
+            <div class="LoginBtn">
+                <el-button type="primary" @click="submitForm(loginFormRef)">
+                    登录
+                </el-button>
+                <el-button type="info" @click="resetForm(loginFormRef)">重置</el-button>
+            </div>
+        </el-form>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, getCurrentInstance, onMounted } from 'vue';
-
+import { FormInstance, ElMessage } from 'element-plus'
+import { Calendar, Search } from '@element-plus/icons-vue'
 const { proxy }: any = getCurrentInstance()
 
+
+// 用户名密码
+const loginRuleForm = reactive({
+    username: 'admin',
+    password: '123456',
+})
+
+const loginFormRef = ref<FormInstance>()
+// 表单验证规则
+const LoginRules = reactive({
+    username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 5, max: 8, message: '用户名为5到8位', trigger: 'blur' },
+    ],
+    password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 6, max: 8, message: '密码为5到8位', trigger: 'blur' },
+    ],
+})
+// 登录事件
 const login = async () => {
-    let res = await proxy.$http.get('/menus')
+    let res = await proxy.$http.post('/login', {
+        username: loginRuleForm.username,
+        password: loginRuleForm.password,
+    })
+    if (res.meta.status !== 200) {
+        return ElMessage.error(res.meta.msg)
+    }
+    const token = res.data.token
+    localStorage.setItem('token', token)
+    ElMessage({
+        type: 'success',
+        message: res.meta.msg,
+        duration: 700,
+        onClose: () => {
+            proxy.$router.push('/home')
+        }
+    })
     console.log(res);
 }
 
-onMounted(() => {
-    login();
-})
+const submitForm = async (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    await formEl.validate((valid, fields) => {
+        if (valid) {
+            login()
+        } else {
+            ElMessage.error('验证不通过请重新输入！')
+        }
+    })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.resetFields()
+}
+
+
+
+
 
 
 
@@ -26,6 +94,26 @@ onMounted(() => {
 .LoginBox {
     width: 100%;
     height: 100%;
-    // background-color: red;
+    background: url(../assets/bj.jpg);
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .el-form {
+        width: 300px;
+        height: 160px;
+        background-color: white;
+        border-radius: 5px;
+        padding: 80px 30px 0;
+        // box-sizing: border-box;
+
+        .LoginBtn {
+            display: flex;
+            justify-content: flex-end;
+
+        }
+
+    }
 }
 </style>
--- src/http/index.ts
+++ src/http/index.ts
@@ -8,11 +8,11 @@ const http = axios.create({
 // 添加请求拦截器
 http.interceptors.request.use(config => {
     // 在发送请求之前做些什么
-    // const token = localStorage.getItem('token');
-    // if (token) {
-    //     config.headers.Authorization = token;
-    //     return config;
-    // }
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = token;
+        return config;
+    }
     return config;
 }, error => {
     // 对请求错误做些什么
