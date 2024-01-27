9bb7eb462f2c889b1f2cc7c226082eedbd8f835c
hanqi <2331129256@qq.com>
2024-01-27 10:45:46 +0800

    welcome页完成

--- src/components/welcome/Welcome.vue
+++ src/components/welcome/Welcome.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="">
-        w
+    <div class="welcomeBox">
+        <img src="../../assets/jinglingBg2.png" alt="">
+
+        <p>欢迎来到<span>精灵购物</span>平台管理系统</p>
     </div>
 </template>
 
@@ -9,4 +11,30 @@ import { ref, reactive } from 'vue';
 
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.welcomeBox {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        width: 50%;
+        flex: 3;
+    }
+
+    p {
+        font-size: 60px;
+        font-weight: 600;
+        color: white;
+        flex: 2;
+
+        span {
+            font-size: 80px;
+            color: #3cd6e3;
+        }
+    }
+}
+</style>
